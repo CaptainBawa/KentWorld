189291b9a8336df44166542a6c69a46fcfae112e
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-25 12:26:12 -0400

    Import LandingPage.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
