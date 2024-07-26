dcc2e3c0332b20eae7de97e470471b163f0c86b3
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-26 13:35:00 -0400

    Add comtact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+const Contact = () => (
+  <section id="contact-us" className="contact">
+    <p>
+      If you have any questions or want to add
+      your business,feel free to reach out to us:
+      Call or Whatsapp
+    </p>
+    <span>
+      <strong>0549542885</strong>
+      {' '}
+      <strong>0549542885</strong>
+    </span>
+  </section>
+);
+
+export default Contact;
