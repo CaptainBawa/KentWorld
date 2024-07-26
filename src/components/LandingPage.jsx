6853148ee9221c2fb6a2864b6df6b5eb6b531b17
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-26 13:36:29 -0400

    Import all component

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,24 @@
+import Business from './Business';
+import NavBar from './NavBar';
+import Hero from './Hero';
+import wave from '../assets/wave.svg';
+import AboutAddBiz from './AboutAddBuz';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const LandingPage = () => (
+  <main>
+    <div className="bg-hero-container" style={{ backgroundImage: `url(${wave})` }}>
+      <NavBar />
+      <Hero />
+    </div>
+    <Business />
+    <AboutAddBiz />
+    <div className="for-space">
+      <Contact />
+    </div>
+    <Footer />
+  </main>
+);
+
+export default LandingPage;
