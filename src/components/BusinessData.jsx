eb4cf11a705a774e31239b42331eaf897892c770
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-26 13:34:20 -0400

    Add business data

---
+++ src/components/BusinessData.jsx
@@ -0,0 +1,95 @@
+import asiedua from '../assets/asiedua.jpg';
+import tiens from '../assets/tiens.jpg';
+import naff from '../assets/naff.jpg';
+import glam from '../assets/glam.jpg';
+import frame from '../assets/frame.jpg';
+import sika from '../assets/sika.jpg';
+import vee from '../assets/vee.jpg';
+import lyns from '../assets/lyns.jpg';
+import kelly from '../assets/kelly.jpg';
+import sparkles from '../assets/sparkles.jpg';
+
+const businessData = [
+  {
+    id: 1,
+    image: asiedua,
+    business_name: 'Asiedua Cleaning Services',
+    location: 'Location',
+    business_description: 'Asiedua Cleaning Services is dedicated to delivering exceptional cleaning solutions for residential and commercial properties. Our professional team is just a call away, ready to provide top-quality cleaning services tailored to your needs.',
+    contact: '0545672516',
+  },
+  {
+    id: 2,
+    image: tiens,
+    business_name: 'GL BAL TIENS Natural Healthcare',
+    location: ' Dunkwa, near Central Market',
+    business_description: 'GL BAL TIENS Natural Healthcare is dedicated to providing specialized treatments for a variety of health issues using natural methods',
+    contact: '0558106331',
+  },
+  {
+    id: 3,
+    image: naff,
+    business_name: 'FuHair Bliss',
+    location: 'Location',
+    business_description: 'Free Melt Band with every frontal wig purchase. Free Plastic Wig Stand with every closure wig purchase',
+    contact: '0557151765',
+  },
+  {
+    id: 4,
+    image: glam,
+    business_name: 'GLAM & COSMETICS',
+    location: 'Dunkwa-On-Offin',
+    business_description: 'Come and experience the essence of class at GLAM & COSMETICS. We are a premier beauty and cosmetics provider, dedicated to offering exceptional services and products to enhance our clients beauty and confidence.',
+    contact: '0247692447',
+  },
+  {
+    id: 5,
+    image: frame,
+    business_name: 'Frame Doc',
+    location: 'Location',
+    business_description: 'Frame Doc specializes in creating and delivering high-quality, custom photo frames that preserve your cherished memories. Whether you are looking for a unique way to display your favorite photos or need a personalized gift, our wide range of frame sizes and styles are designed to meet your needs',
+    contact: '0540306320',
+  },
+  {
+    id: 6,
+    image: sika,
+    business_name: 'Sika&apos;s Beauty Parlou',
+    location: 'Adako Jachie',
+    business_description: 'Welcome to Sika&apos;s Beauty Parlour, where beauty meets excellence. We specialize in providing top-notch beauty services that cater to your every need. Whether you are preparing for a special occasion or simply looking to pamper yourself, our skilled professionals are here to help you look and feel your best.',
+    contact: '0555695821',
+  },
+  {
+    id: 7,
+    image: vee,
+    business_name: 'Vee the Therapist',
+    location: 'Kwadaso, opposite JO & JU',
+    business_description: 'Vee the Therapist is your go-to destination for rejuvenating and therapeutic beauty treatments. Located conveniently in Kwadaso, opposite JO & JU Fuel Station, we specialize in a wide range of services designed to enhance your beauty and well-being. Our experienced therapists are committed to providing exceptional care tailored to meet your individual needs.',
+    contact: '0248829458',
+  },
+  {
+    id: 8,
+    image: lyns,
+    business_name: 'D&apos;Lyns Condos',
+    location: 'Koforidua - Apenkwa',
+    business_description: 'Welcome to D&apos;Lyns Condos, your luxury haven in the heart of Koforidua. We offer top-tier apartments and hotel accommodations designed to provide comfort, elegance, and exceptional service to our guests.',
+    contact: '0539100702',
+  },
+  {
+    id: 9,
+    image: kelly,
+    business_name: 'Kelly&apos;s Apple Phones and Accessories',
+    location: 'Asafo - Kumasi',
+    business_description: 'Kelly&apos;s Apple Phones and Accessories specializes in offering a wide range of iPhones and related accessories. We are dedicated to providing our customers with high-quality products and exceptional service.',
+    contact: '0248891688',
+  },
+  {
+    id: 10,
+    image: sparkles,
+    business_name: 'Sparkles Court',
+    location: 'Location',
+    business_description: 'At Sparkles Court, we specialize in offering a wide range of high-quality perfumes, body sprays, and oil perfumes at affordable prices. Our curated selection ensures that you can find the perfect scent to suit your style and preferences.',
+    contact: '08142602951',
+  },
+];
+
+export default businessData;
