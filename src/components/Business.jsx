5ea18e1ab97372315839459ca5d7b5baf4cb595d
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-28 16:13:19 -0400

    Slice text

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
 import businessData from './BusinessData';
 
 const Business = () => {
-  const businesses = businessData;
+  const [expanded, setExpanded] = useState({});
+
+  const handleToggle = (id) => {
+    setExpanded((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
 
   return (
     <section className="businesses-container">
-      {businesses.map((business) => (
+      {businessData.map((business) => (
         <div className="businesses-sub-container" key={business.id}>
           <img src={business.image} alt={business.business_name} />
           <div className="content">
-            <span>
-              {business.business_name}
-            </span>
+            <span>{business.business_name}</span>
             <span>
               <img id="loc" src="https://img.icons8.com/ultraviolet/40/marker.png" alt="marker" />
               {business.location}
             </span>
-            <p>{business.business_description}</p>
+            <p>
+              {expanded[business.id]
+                ? business.business_description
+                : `${business.business_description.slice(0, 100)}...`}
+              <button onClick={() => handleToggle(business.id)}>
+                {expanded[business.id] ? 'See less' : 'See more'}
+              </button>
+            </p>
             <a href={`tel:${business.contact}`}>Call us</a>
           </div>
         </div>
