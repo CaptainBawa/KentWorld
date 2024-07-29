0a12132f6aaa9d7fc5bcbd6922393e7179545c33
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-29 13:54:28 -0400

    Add new data

--- src/components/BusinessData.jsx
+++ src/components/BusinessData.jsx
@@ -8,6 +8,22 @@ import vee from '../assets/vee.jpg';
 import lyns from '../assets/lyns.jpg';
 import kelly from '../assets/kelly.jpg';
 import sparkles from '../assets/sparkles.jpg';
+import ike from '../assets/ike.jpg';
+import black from '../assets/black.jpg';
+import bass from '../assets/bass.jpg';
+import lamech from '../assets/lamech.jpg';
+import nora from '../assets/nora.jpg';
+import dark from '../assets/dark.jpg';
+import fiz from '../assets/fiz.jpg';
+import est from '../assets/est.jpg';
+import most from '../assets/most.jpg';
+import dkd from '../assets/dkd.jpg';
+import nameit from '../assets/nameit.jpg';
+import dan from '../assets/dan.jpg';
+import oldman from '../assets/oldman.jpg';
+import oldmanshot from '../assets/oldmanshot.jpg';
+import kiki from '../assets/kiki.jpg';
+import blue from '../assets/blue.jpg';
 
 const businessData = [
   {
@@ -90,6 +106,134 @@ const businessData = [
     business_description: 'At Sparkles Court, we specialize in offering a wide range of high-quality perfumes, body sprays, and oil perfumes at affordable prices. Our curated selection ensures that you can find the perfect scent to suit your style and preferences.',
     contact: '08142602951',
   },
+  {
+    id: 11,
+    image: ike,
+    business_name: 'Phone Accessories/Metal Galvanize Plates',
+    location: 'Location',
+    business_description: 'Sell/Swap Phones, Car Running Boards, Metal Galvanize Plates, Round Pipes, etc.',
+    contact: '0557440433',
+  },
+  {
+    id: 12,
+    image: black,
+    business_name: 'Black Royals',
+    location: 'Location',
+    business_description: 'For exceptional service and seamless event coordination, trust Black Royals to make your occasion memorable.',
+    contact: '0202224539',
+  },
+  {
+    id: 13,
+    image: bass,
+    business_name: 'Bassgloss Lipcare Products',
+    location: 'Location',
+    business_description: 'Lipglosses, Lip Scrubs, Lip Masks, Lip Brushes, Lip Oils, Enhance and care for your lips with our luxurious and effective products from Themimi.',
+    contact: '0534678150',
+  },
+  {
+    id: 14,
+    image: lamech,
+    business_name: 'RFFC LAMECH',
+    location: 'Location',
+    business_description: 'Apple Subscription: Apple Music, iCloud Storage. CashApp and PayPal: Buying of Gift Cards available at your own rate, MTN & TIGO Non-Expiry Data, Apple Services Provided: TV, Apple. For reliable subscription services and flexible financial transactions, connect with RFFC LAMECH today.',
+    contact: '0557028405',
+  },
+  {
+    id: 15,
+    image: nora,
+    business_name: 'Rashed by Nara',
+    location: 'Ablekuma-Anyaa, NIC',
+    business_description: 'Connect with Rashed by Nara for exceptional products and services. Reach out to us via phone or follow us on Instagram for updates and promotions.',
+    contact: '0501295629',
+  },
+  {
+    id: 16,
+    image: dark,
+    business_name: 'DARK HUB',
+    location: 'Location',
+    business_description: 'Subscriptions We Offer: Apple Music, Spotify Premium, iCloud Storage, Apple TV, Netflix, Snapchat+, Microsoft Office 365, And more. Stay connected and enjoy premium services with DARK HUB. Contact us for all your subscription needs.',
+    contact: '0550761025',
+  },
+  {
+    id: 17,
+    image: fiz,
+    business_name: 'TrIZTRENDS',
+    location: 'Location',
+    business_description: 'We Offer:  LORIS, AVENTOS, ASRU "AIR", Gaspete, Shoes, Sirens, Slides, And more. For the latest trends and high-quality products, shop now with TrIZTRENDS.',
+    contact: '0593924308',
+  },
+  {
+    id: 18,
+    image: est,
+    business_name: 'EST LOGISTICS',
+    location: 'Location',
+    business_description: 'Our Services:  Import & Export, Clearing & Forwarding, Warehousing, Customs Brokerage, Haulage Solutions, Project Cargo, Door-to-Door Shipping, General Shipping Consultancy. For comprehensive logistics solutions tailored to your needs, trust EST LOGISTICS to deliver excellence every step of the way.',
+    contact: '0208880397',
+  },
+  {
+    id: 19,
+    image: most,
+    business_name: 'MOST ORIGINAL',
+    location: 'Location',
+    business_description: 'Our Services: Gift Card Exchange, Purchase and Sell Bitcoin, Buying Gift Cards, PayPal, Cash App, Skrill Transactions. For reliable gift card exchanges, Bitcoin transactions, and more, connect with MOST ORIGINAL. We ensure secure and efficient services for all your needs.',
+    contact: '0544596326',
+  },
+  {
+    id: 20,
+    image: dkd,
+    business_name: 'DKD',
+    location: 'Dunkwa Mfuom',
+    business_description: 'Our Products: Sewing Materials, Plain T-Shirts and Lacostes, Fabrics (All Kinds). Special Services: Knitting, Overlocking. Minimum Order: 100 Plain T-Shirts. For quality fabrics, sewing materials, and specialized services, choose DKD. We are dedicated to providing the best products and services just for you.',
+    contact: '0540855950',
+  },
+  {
+    id: 21,
+    image: nameit,
+    business_name: 'TRUAME Y',
+    location: 'Location',
+    business_description: 'Our Products: Customized Beads, Customized Necklaces, Customized Bracelets. For personalized and unique jewelry pieces, trust TRUAME Y. Just name it, and we will customize it for you.',
+    contact: '0553591277',
+  },
+  {
+    id: 22,
+    image: dan,
+    business_name: 'DANWILLS SERVICES',
+    location: 'Location',
+    business_description: 'Our Services: Birth Certificate Registration, Replacement of Old Birth Certificates, Re-printing of Birth Certificates, Passport Application/Renewal of Passports. We assist you in acquiring Ghanaian Birth Certificates and Passports without stress. Trust DANWILLS SERVICES for all your registration and application needs.',
+    contact: '0241807661',
+  },
+  {
+    id: 23,
+    image: oldman,
+    business_name: 'Oldman Africa Koncept',
+    location: 'Mankessim - Central Region',
+    business_description: 'Frames: Order, Pay & Receive. Frame Sizes & Prices: (20" x 30") = 250 GHS, (12" x 16") = 100 GHS, (20" x 24") = 220 GHS, (11" x 13") = 70 GHS, (15" x 19") = 150 GHS, (7" x 9") = 35 GHS. For high-quality frames at competitive prices, trust Oldman Africa Koncept. Order now and receive your frames with ease.',
+    contact: '0241021294',
+  },
+  {
+    id: 24,
+    image: oldmanshot,
+    business_name: 'Oldman Africa',
+    location: 'Mankessim - Central Region',
+    business_description: 'Packages: Diamond = 2,500 GHS - (Videography, Photography, Pen Drive, 50 Pieces of Edited & Printed Photos, Ceramic Mug, PhotoBook, A3 Frame, Full Video + Trailer). Gold = 1,900 GHS - (Videography, Photography, Pen Drive, 25 Pieces of Edited & Printed Photos, Ceramic Mug, A3 Frame, Full Video + Trailer). Silver = 1,500 GHS - (Videography, Photography, Pen Drive, 20 Pieces of Edited & Printed Photos, A4 Frame, Traile). Captured Details: The Vows, The First Smile, The Sitting. For unforgettable memories and top-notch event coverage, trust Oldman Africa. Book your next event with us today.',
+    contact: '0241021294',
+  },
+  {
+    id: 25,
+    image: kiki,
+    business_name: 'KIKI',
+    location: 'Takoradi - Kokompe',
+    business_description: 'Services Offered: Pedicure, Manicure, Home Services Available. For professional pedicure and manicure services, visit KIKI or schedule a home service today. Your comfort and satisfaction are our priority.',
+    contact: '0240165505',
+  },
+  {
+    id: 25,
+    image: blue,
+    business_name: "BLUE'S APPAREL",
+    location: 'Kumasi',
+    business_description: 'Products Offered: Clothes, Jewelry, Slippers,Shoes, Quality clothing from SHEIN, Home appliances from China. We deliver nationwide. For stylish apparel and accessories, shop with BLUE APPAREL. Contact us via WhatsApp, Snapchat, or Instagram to place your order today!',
+    contact: '0508469326',
+  }
 ];
 
 export default businessData;
