fdcb0f23090136967706422877f86f3b3575f90e
Collins Bawa <collinskwamebawa@yahoo.com>
2024-07-31 16:09:04 -0400

    Add categories

--- src/components/BusinessData.jsx
+++ src/components/BusinessData.jsx
@@ -37,6 +37,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Asiedua Cleaning Services is dedicated to delivering exceptional cleaning solutions for residential and commercial properties. Our professional team is just a call away, ready to provide top-quality cleaning services tailored to your needs. Our Services: *Residential Cleaning:* Comprehensive cleaning services for homes, including regular housekeeping, deep cleaning, and move-in/move-out cleaning. *Commercial Cleaning:* Office and commercial space cleaning, ensuring a clean and productive environment. *Specialized Cleaning:* Tailored cleaning solutions for unique needs, including post-construction cleaning, carpet cleaning, and window washing. For bookings and more details, visit our website at [www.asieduacleaningservices.com]',
     contact: '0545672516',
+    category: 'Beauty',
   },
   {
     id: 2,
@@ -45,6 +46,7 @@ const businessData = [
     location: ' Dunkwa, near Central Market',
     business_description: 'GL BAL TIENS Natural Healthcare is dedicated to providing specialized treatments for a variety of health issues using natural methods',
     contact: '0558106331',
+    category: 'Health',
   },
   {
     id: 3,
@@ -53,6 +55,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Free Melt Band with every frontal wig purchase. Free Plastic Wig Stand with every closure wig purchase. Instagram: @naffhairbliss',
     contact: '0557151765',
+    category: 'Hairstyles',
   },
   {
     id: 4,
@@ -61,6 +64,7 @@ const businessData = [
     location: 'Dunkwa-On-Offin',
     business_description: 'Come and experience the essence of class at GLAM & COSMETICS. We are a premier beauty and cosmetics provider, dedicated to offering exceptional services and products to enhance our clients beauty and confidence.',
     contact: '0247692447',
+    category: 'Beauty',
   },
   {
     id: 5,
@@ -69,6 +73,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Frame Doc specializes in creating and delivering high-quality, custom photo frames that preserve your cherished memories. Whether you are looking for a unique way to display your favorite photos or need a personalized gift, our wide range of frame sizes and styles are designed to meet your needs',
     contact: '0540306320',
+    category: 'Photography',
   },
   {
     id: 6,
@@ -77,6 +82,7 @@ const businessData = [
     location: 'Adako Jachie',
     business_description: "Welcome to Sika's Beauty Parlour, where beauty meets excellence. We specialize in providing top-notch beauty services that cater to your every need. Whether you are preparing for a special occasion or simply looking to pamper yourself, our skilled professionals are here to help you look and feel your best. Our Services: Wig Fixing: Expert wig installation services for a flawless look. Wig Making: Custom-made wigs crafted to suit your style and preferences. Nails: Comprehensive nail care and styling, including manicures and pedicures. Bridal Makeup: Stunning bridal makeup to make your special day even more memorable. Instagram: [@NampomaahSika](https://www.instagram.com/NampomaahSika)",
     contact: '0555695821',
+    category: 'Hairstyles',
   },
   {
     id: 7,
@@ -85,6 +91,7 @@ const businessData = [
     location: 'Kwadaso, opposite JO & JU',
     business_description: 'Vee the Therapist is your go-to destination for rejuvenating and therapeutic beauty treatments. Located conveniently in Kwadaso, opposite JO & JU Fuel Station, we specialize in a wide range of services designed to enhance your beauty and well-being. Our experienced therapists are committed to providing exceptional care tailored to meet your individual needs. Instagram:[Vee_the_therapist](https://instagram.com/Vee_the_therapist). Our Services: Facials: Rejuvenate your skin with our professional facial treatments. Massage: Relax and unwind with our soothing massage services. Pedicure/Manicure: Complete nail care and styling for hands and feet. Waxing: Smooth and hair-free skin with our expert waxing services. Stretch Marks Removal: Effective treatments to reduce the appearance of stretch marks. Body Polish: Exfoliate and refresh your skin with our body polish treatments. Ear Piercing: Safe and professional ear piercing services. Wood Therapy: Innovative wood therapy for body contouring and cellulite reduction. Eyebrow Tint/Lamination: Enhance and shape your eyebrows with tinting and lamination. Cavitation/Fat Reduction: Non-invasive fat reduction treatments to help you achieve your body goals. Vacuum Butt Lift: Lift and tone with our vacuum butt lift treatments.',
     contact: '0248829458',
+    category: 'Beauty',
   },
   {
     id: 8,
@@ -93,6 +100,7 @@ const businessData = [
     location: 'Koforidua - Apenkwa',
     business_description: "Welcome to D'Lyns Condos, your luxury haven in the heart of Koforidua. We offer top-tier apartments and hotel accommodations designed to provide comfort, elegance, and exceptional service to our guests.Our Services: *Luxury Apartments:* Stylish and comfortable living spaces equipped with modern amenities. *Hotel Accommodations:* Elegant and well-appointed hotel rooms for short-term and long-term stays. Email: dlynscondos@gmail.com",
     contact: '0539100702',
+    category: 'Accommodation',
   },
   {
     id: 9,
@@ -101,6 +109,7 @@ const businessData = [
     location: 'Asafo - Kumasi',
     business_description: "Kelly's Apple Phones and Accessories specializes in offering a wide range of iPhones and related accessories. We are dedicated to providing our customers with high-quality products and exceptional service. Our Products: iPhones: Latest models and various options to suit different needs and budgets. Smart Watches: Cutting-edge smartwatches compatible with iPhones. iPhone Covers: Stylish and protective covers for all iPhone models. iPhone Accessories: A variety of accessories including chargers, earphones, and more.",
     contact: '0248891688',
+    category: 'Phones',
   },
   {
     id: 10,
@@ -109,6 +118,7 @@ const businessData = [
     location: 'Location',
     business_description: 'At Sparkles Court, we specialize in offering a wide range of high-quality perfumes, body sprays, and oil perfumes at affordable prices. Our curated selection ensures that you can find the perfect scent to suit your style and preferences. *Instagram:* [@sparkles_court](https://instagram.com/sparkles_court). Our Products: *Perfumes:* Discover our diverse collection of premium perfumes, including renowned brands like Coco Noir by Chanel Paris. *Body Sprays:* Refresh yourself with our selection of invigorating body sprays. *Oil Perfumes:* Experience the long-lasting and concentrated fragrances of our oil perfumes.',
     contact: '08142602951',
+    category: 'Beauty',
   },
   {
     id: 11,
@@ -117,6 +127,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Our Services: Sell/Swap Phones, Car Running Boards, Metal Galvanize Plates, Round Pipes, etc.',
     contact: '0557440433',
+    category: 'Phones',
   },
   {
     id: 12,
@@ -125,6 +136,7 @@ const businessData = [
     location: 'Location',
     business_description: 'For exceptional service and seamless event coordination, trust Black Royals to make your occasion memorable. Instagram: @blackushering | @blackroyalushering, Email: Hansenbirdget18@gmail.com. Our Services: Ushering, Coordinating, Waiting',
     contact: '0202224539',
+    category: 'Accommodation',
   },
   {
     id: 13,
@@ -133,6 +145,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Our Products: Lipglosses, Lip Scrubs, Lip Masks, Lip Brushes, Lip Oils, Enhance and care for your lips with our luxurious and effective products from Themimi.',
     contact: '0534678150',
+    category: 'Beauty',
   },
   {
     id: 14,
@@ -141,6 +154,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Subscriptions and Services: Apple Subscription: Apple Music, iCloud Storage. CashApp and PayPal: Buying of Gift Cards available at your own rate, MTN & TIGO Non-Expiry Data, Apple Services Provided: TV, Apple. For reliable subscription services and flexible financial transactions, connect with RFFC LAMECH today.',
     contact: '0557028405',
+    category: 'Internet',
   },
   {
     id: 15,
@@ -149,6 +163,7 @@ const businessData = [
     location: 'Ablekuma-Anyaa, NIC',
     business_description: 'Connect with Rashed by Nara for exceptional products and services. Reach out to us via phone or follow us on Instagram for updates and promotions. Instagram: [@akuahn22](https://www.instagram.com/akuahn22)',
     contact: '0501295629',
+    category: 'Beauty',
   },
   {
     id: 16,
@@ -157,6 +172,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Subscriptions We Offer: Apple Music, Spotify Premium, iCloud Storage, Apple TV, Netflix, Snapchat+, Microsoft Office 365, And more. Stay connected and enjoy premium services with DARK HUB. Contact us for all your subscription needs. Instagram: [@dark.fx](https://www.instagram.com/dark.fx) | [@darkforeal](https://www.instagram.com/darkforeal)',
     contact: '0550761025',
+    category: 'Internet',
   },
   {
     id: 17,
@@ -165,6 +181,7 @@ const businessData = [
     location: 'Location',
     business_description: 'We Offer:  LORIS, AVENTOS, ASRU "AIR", Gaspete, Shoes, Sirens, Slides, And more. For the latest trends and high-quality products, shop now with TrIZTRENDS. Instagram: [@fiztrends](https://www.instagram.com/fiztrends)',
     contact: '0593924308',
+    category: 'Phones',
   },
   {
     id: 18,
@@ -173,6 +190,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Our Services:  Import & Export, Clearing & Forwarding, Warehousing, Customs Brokerage, Haulage Solutions, Project Cargo, Door-to-Door Shipping, General Shipping Consultancy. For comprehensive logistics solutions tailored to your needs, trust EST LOGISTICS to deliver excellence every step of the way. Email: estlogistics8@gmail.com',
     contact: '0208880397',
+    category: 'Accommodation',
   },
   {
     id: 19,
@@ -181,6 +199,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Our Services: Gift Card Exchange, Purchase and Sell Bitcoin, Buying Gift Cards, PayPal, Cash App, Skrill Transactions. For reliable gift card exchanges, Bitcoin transactions, and more, connect with MOST ORIGINAL. We ensure secure and efficient services for all your needs.',
     contact: '0544596326',
+    category: 'Internet',
   },
   {
     id: 20,
@@ -189,6 +208,7 @@ const businessData = [
     location: 'Dunkwa Mfuom',
     business_description: 'Our Products: Sewing Materials, Plain T-Shirts and Lacostes, Fabrics (All Kinds). Special Services: Knitting, Overlocking. Minimum Order: 100 Plain T-Shirts. For quality fabrics, sewing materials, and specialized services, choose DKD. We are dedicated to providing the best products and services just for you.',
     contact: '0540855950',
+    category: 'Fashion',
   },
   {
     id: 21,
@@ -197,6 +217,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Our Products: Customized Beads, Customized Necklaces, Customized Bracelets. For personalized and unique jewelry pieces, trust TRUAME Y. Just name it, and we will customize it for you.',
     contact: '0553591277',
+    category: 'Fashion',
   },
   {
     id: 22,
@@ -205,6 +226,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Our Services: Birth Certificate Registration, Replacement of Old Birth Certificates, Re-printing of Birth Certificates, Passport Application/Renewal of Passports. We assist you in acquiring Ghanaian Birth Certificates and Passports without stress. Trust DANWILLS SERVICES for all your registration and application needs.',
     contact: '0241807661',
+    category: 'Documents',
   },
   {
     id: 23,
@@ -213,6 +235,7 @@ const businessData = [
     location: 'Mankessim - Central Region',
     business_description: 'Frames: Order, Pay & Receive. Frame Sizes & Prices: (20" x 30") = 250 GHS, (12" x 16") = 100 GHS, (20" x 24") = 220 GHS, (11" x 13") = 70 GHS, (15" x 19") = 150 GHS, (7" x 9") = 35 GHS. For high-quality frames at competitive prices, trust Oldman Africa Koncept. Order now and receive your frames with ease. Instagram: [@Oldman_Africa](https://www.instagram.com/Oldman_Africa)',
     contact: '0241021294',
+    category: 'Photography',
   },
   {
     id: 24,
@@ -221,6 +244,7 @@ const businessData = [
     location: 'Mankessim - Central Region',
     business_description: 'Packages: Diamond = 2,500 GHS - (Videography, Photography, Pen Drive, 50 Pieces of Edited & Printed Photos, Ceramic Mug, PhotoBook, A3 Frame, Full Video + Trailer). Gold = 1,900 GHS - (Videography, Photography, Pen Drive, 25 Pieces of Edited & Printed Photos, Ceramic Mug, A3 Frame, Full Video + Trailer). Silver = 1,500 GHS - (Videography, Photography, Pen Drive, 20 Pieces of Edited & Printed Photos, A4 Frame, Traile). Captured Details: The Vows, The First Smile, The Sitting. For unforgettable memories and top-notch event coverage, trust Oldman Africa. Book your next event with us today. Website: (http://www.oldman.com) | Email: korankyiedonkoh@gmail.com | Instagram: [@Oldman_Africa](https://www.instagram.com/Oldman_Africa) | Facebook: [Oldman](https://www.facebook.com/Oldman)',
     contact: '0241021294',
+    category: 'Photography',
   },
   {
     id: 25,
@@ -229,6 +253,7 @@ const businessData = [
     location: 'Takoradi - Kokompe',
     business_description: 'Services Offered: Pedicure, Manicure, Home Services Available. For professional pedicure and manicure services, visit KIKI or schedule a home service today. Your comfort and satisfaction are our priority.',
     contact: '0240165505',
+    category: 'Beauty',
   },
   {
     id: 26,
@@ -237,6 +262,7 @@ const businessData = [
     location: 'Kumasi',
     business_description: 'Products Offered: Clothes, Jewelry, Slippers,Shoes, Quality clothing from SHEIN, Home appliances from China. We deliver nationwide. For stylish apparel and accessories, shop with BLUE APPAREL. Contact us via WhatsApp, Snapchat, or Instagram to place your order today!',
     contact: '0508469326',
+    category: 'Fashion',
   },
   {
     id: 27,
@@ -245,6 +271,7 @@ const businessData = [
     location: 'Assin Foso. Kumasi - Ahodwo',
     business_description: 'OUR SERVICES: Birth Certificate & Passport Application, Study Abroad Programs, Car Rental, Holidays & Tours, VISA Application Assistance, Work Opportunities in Dubai, Kuwait, Saudi Arabia, Qatar, etc., Flight Bookings, Travel Insurance, VISA Appointment Bookings, Domestic & International Tours. Email: richotravelagency@gmail.com',
     contact: '0245215383',
+    category: 'Accommodation',
   },
   {
     id: 28,
@@ -253,6 +280,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Main Dishes: Jollof Rice with Chicken/Fish/Pork: GHC 45, Assorted Jollof Rice with Chicken/Fish/Pork: GHC 50, Car Rental, Fried Rice with Chicken: GHC 40, Assorted Fried Rice with Chicken: GHC 45, Fantasy Angwamo with Beef, Fried Egg, Sardine, and Sausage: GHC 35. Special Orders: Fufu with Light/Groundnut Soup (Chicken/Cow Meat): GHC 55, Omotuo with Groundnut Soup (Cow Meat/Fish): GHC 55. Snacks: Sobolo, Spring Rolls, and Samosa: GHC 20. *Note:* Comes with free mint toffee or chewing gum.',
     contact: '0555695821',
+    category: 'Food',
   },
   {
     id: 29,
@@ -261,6 +289,7 @@ const businessData = [
     location: 'Asankragwa Motta',
     business_description: 'Menu: Aponkye Jollof: Served with Plantain/Goat, Salad/Shito - **GHS 70*',
     contact: '0249796504',
+    category: 'Food',
   },
   {
     id: 30,
@@ -269,6 +298,7 @@ const businessData = [
     location: 'Location',
     business_description: 'Small Bundles (No Expiry): 1GB - GHC 6, 2GB - GHC 11, 3GB - GHC 17, 4GB - GHC 21, 5GB - GHC 27, 6GB - GHC 32, 8GB - GHC 39, 10GB - GHC 47, 15GB - GHC 69, 20GB - GHC 89. Big Bundles: 30GB - GHC 128, 40GB - GHC 170, 50GB - GHC 209',
     contact: '0534210817',
+    category: 'Internet',
   }
 ];
 
