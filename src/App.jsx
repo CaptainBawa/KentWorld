773def3faeaf631677cf7d8ad7032ec488fcb4f4
Collins Bawa <collinskwamebawa@yahoo.com>
2024-08-04 06:41:56 -0400

    Import BusinessDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-// import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import BusinessDetails from './components/BusinessDetails';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/business/:contact" element={<BusinessDetails />} />
     </Routes>
   </BrowserRouter>
 );
